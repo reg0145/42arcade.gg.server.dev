23115577d64bf7cc45d4d4a1f03b72875b728270
donghyuk <reg0145@naver.com>
2022-11-29 19:11:24 +0900

    done

--- src/main/java/io/pp/arcade/v1/domain/currentmatch/controller/CurrentMatchController.java
+++
@@ -1,9 +0,0 @@
-package io.pp.arcade.v1.domain.currentmatch.controller;
-
-import io.pp.arcade.v1.domain.currentmatch.dto.CurrentMatchResponseDto;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface CurrentMatchController {
-    CurrentMatchResponseDto currentMatchFind(HttpServletRequest request);
-}
--- src/main/java/io/pp/arcade/v1/domain/rank/type/RankType.java
+++
@@ -1,5 +0,0 @@
-package io.pp.arcade.v1.domain.rank.type;
-
-public class RankType {
-    public static final Integer UNRANK = -1;
-}
